66139b7868b8d7341557199bbe42e02a92941d1c
Cecily Grossmann <grssmnn2@gmail.com>
2018-03-15 10:23:12 -0500

    trying to fix buttons

--- server.js
+++ server.js
@@ -36,8 +36,9 @@ var PORT = process.env.PORT || 3000;
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function(){
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+      console.log("App listening on PORT " + PORT);
   });
-});
+}
+)
